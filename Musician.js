49a22b822e9a88f913057bcb4e3b5fd8d05a466d
aamcodes <aaronmendezcodes@gmail.com>
2023-03-01 14:13:35 -0500

    Finished creating models for both the musician and the band database columns.

--- Musician.js
+++ Musician.js
@@ -1,8 +1,11 @@
-const {Sequelize, sequelize} = require('./db');
+const { Sequelize, sequelize } = require('./db');
 
 // TODO - define the Musician model
-let Musician;
+let Musician = sequelize.define('musicians', {
+	name: Sequelize.STRING,
+	instrument: Sequelize.STRING,
+});
 
 module.exports = {
-    Musician
+	Musician,
 };
